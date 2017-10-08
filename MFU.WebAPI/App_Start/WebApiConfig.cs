bb1e8f9f0a82fba77c961636d3ea55c7eb037a4a
ppraserts <ppraserts@gmail.com>
2017-10-09 01:04:12 +0700

    add JWT

--- MFU.WebAPI/App_Start/WebApiConfig.cs
+++ MFU.WebAPI/App_Start/WebApiConfig.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MFU.WebAPI.Security;
+using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -10,9 +12,17 @@ namespace MFU.WebAPI
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
-
             // Web API routes
             config.MapHttpAttributeRoutes();
+
+            // Configure the authentication filter to run on every request marked with the AuthorizeAttribute
+            config.Filters.Add(new BearerAuthenticationFilter());
+
+            // Configure the sliding expiration handler to run on every request
+            config.MessageHandlers.Add(new SlidingExpirationHandler());
+
+            // Help our JSON look professional using camelCase
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
---
+++ MFU.WebAPI/Controllers/Apis/TokenController.cs
@@ -0,0 +1,111 @@
+﻿using MFU.WebAPI.Security;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Configuration;
+using System.Web.Http;
+
+namespace MFU.WebAPI.Controllers
+{
+    public class Credentials
+    {
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+
+    public class User
+    {
+        public long UserId { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+    }
+    public class TokenController : BaseApiController
+    {
+        private List<User> users = new List<User>() {
+                                                  new User  {
+                                                        UserId= 3001,
+                                                        FullName= "Liz Lemon",
+                                                        EmailAddress= "liz.lemon@example.com",
+                                                        Password= "Password1"
+                                                      },
+                                                  new User {
+                                                        UserId= 3002,
+                                                        FullName= "Jack Donaghy",
+                                                        EmailAddress= "jack.donaghy@example.com",
+                                                        Password= "Password1"
+                                                  },
+                                                  new User {
+                                                        UserId= 3003,
+                                                        FullName= "Tracy Jordan",
+                                                        EmailAddress= "tracy.jordan@example.com",
+                                                        Password= "Password1"
+                                                  }
+                                                };
+
+        public IHttpActionResult Post(Credentials credential)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            // It goes without question that in the real world our passwords would be hashed
+            var user = users.FirstOrDefault(u => u.EmailAddress.Equals(credential.EmailAddress, StringComparison.OrdinalIgnoreCase) && credential.Password == u.Password);
+            if (user == null)
+                return Unauthorized();
+
+            var lifetimeInMinutes = int.Parse(WebConfigurationManager.AppSettings["TokenLifetimeInMinutes"]);
+            var token = CreateToken(user.UserId.ToString(), user.FullName, lifetimeInMinutes);
+
+            return Ok(new
+            {
+                Token = token,
+                LifetimeInMinutes = lifetimeInMinutes,
+                FullName = user.FullName
+                // Any anything else that you want here...
+            });
+        }
+
+        [Authorize]
+        public async Task<IHttpActionResult> Get()
+        {
+            return Ok("Hello " + User.Identity.Name + "!!!");
+        }
+
+        public static string CreateToken(string userId, string fullName, int lifetimeInMinutes)
+        {
+            // Create the JWT
+            var claimsIdentity = new ClaimsIdentity(new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, userId),
+                new Claim("name", fullName)
+                // And any other bit of (session) data you want....
+            });
+
+            var now = DateTime.UtcNow;
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = claimsIdentity,
+                Issuer = SecurityConfiguration.TokenIssuer,
+                Audience = SecurityConfiguration.TokenAudience,
+                SigningCredentials = SecurityConfiguration.SigningCredentials,
+                IssuedAt = now,
+                Expires = now.AddMinutes(lifetimeInMinutes)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
